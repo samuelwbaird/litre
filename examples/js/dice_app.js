447a524b6c99eb8da595e3f84bab9da39fbe27eb
Samuel Baird <samuelwbaird@gmail.com>
2022-06-19 23:20:54 +1000

    dom screen concept
    Designated "screen" element (a div) that can run a logical scaled co-ordinate system within the window size.

---
+++ examples/js/dice_app.js
@@ -0,0 +1,15 @@
+import { resource, sequence, state, dom, app_node } from '../../lib/litre/litre.js';
+
+class dice_app extends app_node {
+
+	begin () {
+		super.begin();
+	}
+
+	update () {
+		super.update();
+	}
+
+}
+
+export default dice_app;
