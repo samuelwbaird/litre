d9b8edf92afe9cb2290f12b4b300084f8a204d93
Samuel Baird <samuelwbaird@gmail.com>
2022-06-19 15:22:15 +1000

    set up modules
    port update, dispatch, tweening and coroutine to the sequence module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,57 @@
-'env': {
-	'browser': true,
-	'es6': true
-},
-'extends': 'eslint:recommended',
-'parserOptions': {
-	'ecmaVersion': 2020,
-	'sourceType': 'module',
-	'ecmaFeatures': {
-		'impliedStrict': true,
-	}
-},
-'rules': {
-	'indent': [
-		'error',
-		'tab'
-	],
-	'linebreak-style': [
-		'error',
-		'unix'
-	],
-	'quotes': [
-		'error',
-		'single'
-	],
-	'semi': [
-		'error',
-		'always'
-	],
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true
+	},
 
-	'array-bracket-spacing': ['error', 'never'],
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 2020,
+		'sourceType': 'module',
+		'ecmaFeatures': {
+			'impliedStrict': true,
+		}
+	},
+
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+
+		'array-bracket-spacing': ['error', 'never'],
 	
-	'block-spacing': ['error', 'always'],
-	'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+		'block-spacing': ['error', 'always'],
+		'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
 	
-	'comma-dangle': ['error', 'always-multiline'],
-	'comma-spacing': ['error', { "before": false, "after": true }],
-	'new-parens': 'error',
-	'no-trailing-spaces': 'error',
-	'no-multiple-empty-lines': 'error',
-	'space-in-parens': 'error',
-	'space-before-function-paren': 'error',
-	'space-before-blocks': 'error',
+		'comma-dangle': ['error', 'always-multiline'],
+		'comma-spacing': ['error', { "before": false, "after": true }],
+		'new-parens': 'error',
+		'no-trailing-spaces': 'error',
+		'no-multiple-empty-lines': 'error',
+		'space-in-parens': 'error',
+		'space-before-function-paren': 'error',
+		'space-before-blocks': 'error',
 	
-	'arrow-body-style': ["error", "always"],
-	'arrow-parens': 'error',
-	'arrow-spacing': 'error',
-	'prefer-arrow-callback': 'error',
-	'prefer-const': 'error',
-	'no-loop-func': 'error',
-	'no-var': 'error',
+		'arrow-body-style': ["error", "always"],
+		'arrow-parens': 'error',
+		'arrow-spacing': 'error',
+		'prefer-arrow-callback': 'error',
+		'prefer-const': 'error',
+		'no-loop-func': 'error',
+		'no-var': 'error',
 		
-}
+	}
+};
